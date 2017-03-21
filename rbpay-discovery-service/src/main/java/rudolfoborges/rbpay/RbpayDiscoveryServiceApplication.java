7f1568e90f79a68074ce7a1368b1a2dd562e5529
Rudolfo Borges <oliveira.rudolfo@gmail.com>
2017-03-20 23:48:26 -0300

    Criacao do servico de discovery e register com Netflix Eureka

---
+++ rbpay-discovery-service/src/main/java/rudolfoborges/rbpay/RbpayDiscoveryServiceApplication.java
@@ -0,0 +1,12 @@
+package rudolfoborges.rbpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RbpayDiscoveryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RbpayDiscoveryServiceApplication.class, args);
+	}
+}
