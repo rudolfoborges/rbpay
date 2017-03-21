be7d7ecd64267825aee7f8e6735b7a477c540b3d
Rudolfo Borges <oliveira.rudolfo@gmail.com>
2017-03-20 23:44:07 -0300

    Criação do microservico para produtos

---
+++ rbpay-product/src/main/java/rudolfoborges/rbpay/RbpayProductApplication.java
@@ -0,0 +1,12 @@
+package rudolfoborges.rbpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RbpayProductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RbpayProductApplication.class, args);
+	}
+}
