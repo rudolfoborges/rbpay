eb6135056ade605cc123c1d55db62b0abe969236
Rudolfo Borges <cin_ruoliveira@uolinc.com>
2017-03-21 13:23:34 -0300

    Feature Product

--- rbpay-discovery-service/src/main/java/rudolfoborges/rbpay/RBPayDiscoveryServiceApplication.java
+++ rbpay-discovery-service/src/main/java/rudolfoborges/rbpay/RBPayDiscoveryServiceApplication.java
@@ -8,7 +8,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @EnableEurekaServer
 public class RBPayDiscoveryServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RBPayDiscoveryServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(RBPayDiscoveryServiceApplication.class, args);
+    }
 }
